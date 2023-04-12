8d0ed8eb5d735ecafbd629cd9ebb1ba51e355378
Zhibek <zhibekrahym05@gmail.com>
2023-04-12 15:03:56 +0600

    final commit

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ let slots = [
     }
 ];
 
-app.get('/api/slots', (req, res) => {
+app.get('/slots', (req, res) => {
     res.status(200).json(slots);
 });
 
-app.post('/api/slot', (req, res) => {
+app.post('/slot', (req, res) => {
     const newProduct = req.body;
     slots.push(newProduct);
 
